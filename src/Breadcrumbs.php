494b61dad614f81e97d948a337b5db9ff08d778e
Takashi Kitajima <inc@2inc.org>
2017-07-16 13:19:52 +0900

    Some fixes

--- src/Breadcrumbs.php
+++ src/Breadcrumbs.php
@@ -1,31 +1,6 @@
 <?php
 namespace Inc2734\WP_Breadcrumbs;
 
-/**
- * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
- */
-class Breadcrumbs {
-
-	/**
-	 * $WP_Breadcrumbs
-	 * @var Inc2734_WP_Breadcrumbs
-	 */
-	protected $WP_Breadcrumbs;
-
-	/**
-	 * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-	 */
-	public function __construct() {
-		include_once( __DIR__ . '/wp-breadcrumbs.php' );
-		$this->WP_Breadcrumbs = new \Inc2734_WP_Breadcrumbs();
-	}
-
-	/**
-	 * Gets breadcrumbs items
-	 *
-	 * @return array
-	 */
-	public function get() {
-		return $this->WP_Breadcrumbs->get();
-	}
+include_once( __DIR__ . '/wp-breadcrumbs.php' );
+class Breadcrumbs extends \Inc2734_WP_Breadcrumbs {
 }
